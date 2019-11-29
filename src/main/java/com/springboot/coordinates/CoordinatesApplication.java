74df556a604b064bd82651a08eea61ae50fdfc7c
yangguotao <574852797@qq.com>
2019-11-29 17:46:16 +0800

    1

---
+++ src/main/java/com/springboot/coordinates/CoordinatesApplication.java
@@ -0,0 +1,17 @@
+package com.springboot.coordinates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: yangguotao
+ * @Date: 2019/11/29
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class CoordinatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoordinatesApplication.class, args);
+    }
+}
